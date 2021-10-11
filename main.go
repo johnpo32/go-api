349b5f3d8a5c905271fd27ee907fbb8d6961f315
johnpo32 <johnchaux@gmail.com>
2021-10-10 20:36:45 -0500

    Add fiber

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"api/database"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	database.Connect()
+
+	app := fiber.New()
+	log.Fatal(app.Listen("127.0.0.1:3000"))
 }
